2814b20bd2d648796b87670aad2fb986c285ff69
zhecks <hecks.zhu@gmail.com>
2023-05-24 14:55:26 +0800

    feat(*): test

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import * as github from '@actions/github'
 async function run(): Promise<void> {
   try {
     console.log(github.context)
+    console.log("test")
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
