2659bfe5a743c738da07212a164f29834ab08f4d
jiuhuche120 <760297795@qq.com>
2023-05-25 03:13:05 +0800

    feat(*): suport polling wait other actions

---
+++ src/wait.ts
@@ -0,0 +1,61 @@
+import {context} from '@actions/github'
+import * as httpm from '@actions/http-client'
+
+async function wait(milliseconds: number): Promise<string> {
+    return new Promise(resolve => {
+        if (isNaN(milliseconds)) {
+            throw new Error('milliseconds not a number')
+        }
+        setTimeout(() => resolve('done!'), milliseconds)
+    })
+}
+
+interface Options {
+    timeoutSeconds: number
+    intervalSeconds: number
+}
+
+interface workflowRuns {
+    workflow_runs: workflowRun[]
+}
+
+interface workflowRun {
+    status: string
+    conclusion: string
+}
+
+export async function polling(options: Options): Promise<string> {
+    const {timeoutSeconds, intervalSeconds} = options
+    let now = new Date().getTime()
+    const deadline = now + timeoutSeconds * 1000
+    const headSha = context.payload.pull_request?.head.sha
+    let isCompleted = true
+    let isSuccess = true
+    const http = new httpm.HttpClient('lark-pr-notify-action')
+    const url = `${context.apiUrl}/repos/${context.repo.owner}/${context.repo.repo}/actions/runs?head_sha=${headSha}`
+    while (now < deadline) {
+        console.log(url)
+        const response = await http.get(url)
+        const body = await response.readBody()
+        const workflows: workflowRuns = JSON.parse(body)
+        console.log(body)
+        for (const workflow of workflows.workflow_runs) {
+            if (workflow.status !== 'completed') {
+                isCompleted = false
+            }
+            if (workflow.conclusion === 'failure') {
+                isSuccess = false
+            }
+        }
+        if (isCompleted) {
+            break
+        }
+        await wait(intervalSeconds * 1000)
+        now = new Date().getTime()
+    }
+    if (now >= deadline || !isSuccess) {
+        return 'failure'
+    } else {
+        return 'success'
+    }
+}
